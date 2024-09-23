bd78729d3a6a98c92909d49def5157816d86003f
Ahmed Hatem <ahmehatem32@gmail.com>
2024-09-23 11:43:50 -0700

    Added Array-Based Queue

---
+++ Queue/arrayBasedQueue.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Queue {
+	int size { };
+	int front { 0 };
+	int rear { 0 };
+	int added_elements { };
+	int *array { };
+
+public:
+	Queue(int size) :
+			size(size) {
+		array = new int[size];
+	}
+
+	~Queue() {
+		delete[] array;
+	}
+
+	int next(int pos) {
+		++pos;
+		if (pos == size)
+			pos = 0;
+		return pos;
+		//return (pos + 1) % size;	//  Or shorter way
+	}
+
+	void enqueue(int value) {
+		assert(!isFull());
+		array[rear] = value;
+		rear = next(rear);
+		added_elements++;
+	}
+
+	int dequeue() {
+		assert(!isEmpty());
+		int value = array[front];
+		front = next(front);
+		--added_elements;
+		return value;
+	}
+
+	void display() {
+		cout << "Front " << front << " - rear " << rear << "\t";
+		if (isFull())
+			cout << "full";
+		else if (isEmpty()) {
+			cout << "empty\n\n";
+			return;
+		}
+		cout << "\n";
+
+		for (int cur = front, step = 0; step < added_elements; ++step, cur = next(cur))
+			cout << array[cur] << " ";
+		cout << "\n\n";
+	}
+
+	int isEmpty() {
+		return added_elements == 0;
+	}
+
+	bool isFull() {
+		return added_elements == size;
+	}
+};
+
+int main() {
+	
+	}
