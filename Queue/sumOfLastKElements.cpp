35905114eb03f9d8ce6e0b4636d5ee20fadb7c87
Ahmed Hatem <ahmehatem32@gmail.com>
2024-09-25 00:09:01 -0700

    Added sum of last K elements

---
+++ Queue/sumOfLastKElements.cpp
@@ -0,0 +1,87 @@
+// That is just sliding window using Queue
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Queue {
+	int size { };
+	int front { 0 };
+	int rear { 0 };
+	int added_elements { };
+	int *array { };
+
+public:
+	Queue(int size) :
+			size(size) {
+		array = new int[size];
+	}
+
+	~Queue() {
+		delete[] array;
+	}
+
+	int next(int pos) {
+		++pos;
+		if (pos == size)
+			pos = 0;
+		return pos;
+	}
+
+	void enqueue(int value) {
+		assert(!isFull());
+		array[rear] = value;
+		rear = next(rear);
+		added_elements++;
+	}
+
+	int dequeue() {
+		assert(!isEmpty());
+		int value = array[front];
+		front = next(front);
+		--added_elements;
+		return value;
+	}
+
+	void display() {
+		cout << "Front " << front << " - rear " << rear << "\t";
+		if (isFull())
+			cout << "full";
+		else if (isEmpty()) {
+			cout << "empty\n\n";
+			return;
+		}
+		cout << "\n";
+
+		for (int cur = front, step = 0; step < added_elements; ++step, cur = next(cur))
+			cout << array[cur] << " ";
+		cout << "\n\n";
+	}
+
+	int isEmpty() {
+		return added_elements == 0;
+	}
+
+	bool isFull() {
+		return added_elements == size;
+	}
+};
+class Last_K_numbers_sum_stream {
+    int k;
+    int sum { };
+    int added_elements;
+    Queue q;
+public:
+    Last_K_numbers_sum_stream(int k) : k(k), q(k+1) {
+    }
+
+    int next(int new_num) {
+        q.enqueue(new_num);
+        added_elements++;
+        
+        if (added_elements > k) {
+            sum -= q.dequeue();
+            added_elements--;
+        }
+        return sum;
+    }
+};
