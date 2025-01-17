4aa5b383425f605cca76af8978e5d80b86eb37ca
Ahmed Hatem <ahmehatem32@gmail.com>
2025-01-17 15:08:33 +0300

    Added minimum() and two more functions to help with the process

--- Tree/binarySearchTree.cpp
+++ Tree/binarySearchTree.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 class BinarySearchTree {
-private:
     int data { };
     BinarySearchTree* left { };
     BinarySearchTree* right { };
 
-public:
+    BinarySearchTree* searchNode(const int target) {
+        if (!this)
+            return nullptr;
 
-    explicit BinarySearchTree(int data) :
+        if (target == data)
+            return this;
+
+        if (target < data)
+             return left->searchNode(target);
+
+        return right->searchNode(target);
+    }
+
+    static BinarySearchTree* _minimum(BinarySearchTree *subTree) {
+        if (subTree->left)
+            return _minimum(subTree->left);
+        return subTree;
+    }
+public:
+    explicit BinarySearchTree(int const data) :
             data(data) {
     }
 
@@ -21,7 +37,7 @@ public:
             right->print_inorder();
     }
 
-    bool search(int target) {	// tail recursion
+    bool search(int const target) {
         if (target == data)
             return true;
 
@@ -31,8 +47,50 @@ public:
         return right && right->search(target);
     }
 
+    void insert(int const target) {
+        if (target < data) {
+            if (!left)
+                left = new BinarySearchTree(target);
+            else
+                left->insert(target);
+        }   else if (target > data) {
+            if(!right)
+                right = new BinarySearchTree(target);
+            else
+                right->insert(target);
+        }   // else : already exists
+
+    }
+
+    BinarySearchTree* minimum(const int target) {
+        BinarySearchTree *subTree = searchNode(target);
+        if (subTree == nullptr)
+            return nullptr;
+        return _minimum(subTree);
+    }
+
+    friend  void printData(const BinarySearchTree* tree);
 };
 
+void printData(const BinarySearchTree* tree) {
+    if (tree == nullptr)
+        cout << "There is no node with this value!";
+    else
+        cout << tree->data;
+}
+
+
 int main() {
-    cout << "Hello";
+    BinarySearchTree tree(50);
+    tree.insert(20);
+    tree.insert(45);
+    tree.insert(70);
+    tree.insert(73);
+    tree.insert(35);
+    tree.insert(15);
+    tree.insert(60);
+
+    cout << "bye";
+
+    return 0;
 }
